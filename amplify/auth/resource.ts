eff385a10c56b88af683c23d78838865f8d2ef28
ronaldolimacloud <ronaldolimacloud@gmail.com>
2025-08-19 16:19:27 +1000

    Adding_google_001

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,23 @@
 // amplify/auth/resource.ts
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
 export const auth = defineAuth({
   loginWith: {
     email: true,
-
-
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+        scopes: ['openid', 'email', 'profile'],
+        attributeMapping: {
+          email: 'email',
+          givenName: 'given_name',
+          familyName: 'family_name',
+        },
+      },
+      callbackUrls: ["amplifycognitotemplate://callback/"],
+      logoutUrls: ["amplifycognitotemplate://signout/"],
+    },
   },
   userAttributes: {
     email: { required: true },
